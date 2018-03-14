8d66518e0f0da5aabc4eb95e192b07be980ff84e
lpc <974123274@qq.com>
2018-03-15 00:54:33 +0800

    Fix indent

--- insert_sort.py
+++ insert_sort.py
@@ -1,22 +1,22 @@
 def insert_sort(A):
-	for j in range(1,len(A)):
-		key = A[j]
-		i = j-1
-		while i >= 0 and A[i] < key:
-			A[i+1] = A[i]
-			i -= 1
-		A[i+1] = key
+    for j in range(1,len(A)):
+        key = A[j]
+        i = j-1
+        while i >= 0 and A[i] < key:
+            A[i+1] = A[i]
+            i -= 1
+        A[i+1] = key
 
 def insert_sort_rec(A):
-	'''插入排序的递归实现'''
-	_insert(A, 0, len(A))
+    '''插入排序的递归实现'''
+    _insert(A, 0, len(A))
 
 def _insert(A, beg, end):
-	if beg < end - 1:
-		_insert(A, beg, end-1)
-		key = A[end-1]
-		i = end - 1
-		while i > beg and A[i-1] >= key:
-			A[i] = A[i-1]
-			i -= 1
-          A[i] = key
+    if beg < end - 1:
+        _insert(A, beg, end-1)
+        key = A[end-1]
+        i = end - 1
+        while i > beg and A[i-1] >= key:
+            A[i] = A[i-1]
+            i -= 1
+        A[i] = key
--- merge_sort.py
+++ merge_sort.py
@@ -1,49 +1,49 @@
 # coding:utf-8
 
 def merge(l,p,q,r):
-	'''合并两个已排序的子序列，产生一个排好序的新序列(in-place).
+    '''合并两个已排序的子序列，产生一个排好序的新序列(in-place).
 
-	给定一个序列l, p,q,r是序列下标，满足p<=q<r,假定子序列l[p:q+1],
-	l[q+1:r]按升序排列。
+    给定一个序列l, p,q,r是序列下标，满足p<=q<r,假定子序列l[p:q+1],
+    l[q+1:r]按升序排列。
 
-	参数：
-		l: 待排序的序列
-		p: 序列的起始位置
-		q: 满足使l[p:q+1],l[q+1:r]有序的位置
-		r: 序列的结束位置(不包括)
+    参数：
+        l: 待排序的序列
+        p: 序列的起始位置
+        q: 满足使l[p:q+1],l[q+1:r]有序的位置
+        r: 序列的结束位置(不包括)
 
-	返回:
-		无。函数调用结果是序列l[p:r]有序。
-	'''
-	a = l[p:q+1]
-	b = l[q+1:r]
-	i = 0
-	j = 0
+    返回:
+        无。函数调用结果是序列l[p:r]有序。
+    '''
+    a = l[p:q+1]
+    b = l[q+1:r]
+    i = 0
+    j = 0
 
-	for k in range(p,r):
-		if i == q+1-p or j == r-q-1:
-			break
-		if a[i] < b[j]:
-			l[k] = a[i]
-			i += 1
-		else:
-			l[k] = b[j]
-			j += 1
+    for k in range(p,r):
+        if i == q+1-p or j == r-q-1:
+            break
+        if a[i] < b[j]:
+            l[k] = a[i]
+            i += 1
+        else:
+            l[k] = b[j]
+            j += 1
 
-	if i == q+1-p:
-		l[k:r] = b[j:len(b)]
-	else:
-		l[k:r] = a[i:len(a)]
+    if i == q+1-p:
+        l[k:r] = b[j:len(b)]
+    else:
+        l[k:r] = a[i:len(a)]
 
 
 def _merge_sort(l,p,r):
-	if p < r-1:
-		q = (p+r)//2
-		_merge_sort(l,p,q)
-		_merge_sort(l,q,r)
-		merge(l,p,q-1,r)
+    if p < r-1:
+        q = (p+r)//2
+        _merge_sort(l,p,q)
+        _merge_sort(l,q,r)
+        merge(l,p,q-1,r)
 
 
 
 def merge_sort(l):
-	return _merge_sort(l,0,len(l))
+    return _merge_sort(l,0,len(l))
